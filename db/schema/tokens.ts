277ff9974b970ada25dfc8ebdb229d9ae29c1c8a
nkrivous <kolsln@list.ru>
2024-05-27 18:59:54 +1000

    Update db schema

--- db/schema/tokens.ts
+++ db/schema/tokens.ts
@@ -1,20 +1,14 @@
-import {
-  index,
-  integer,
-  pgTable,
-  serial,
-  unique,
-  varchar,
-} from "drizzle-orm/pg-core";
+import { index, integer, pgTable, serial, unique } from "drizzle-orm/pg-core";
 import { users } from "./users";
 import { relations } from "drizzle-orm";
+import { aiModelEnum } from "./enums";
 
 export const tokens = pgTable(
   "tokens",
   {
     id: serial("id").primaryKey(),
-    tokens: integer("tokens").notNull(),
-    aiModel: varchar("ai_model", { length: 100 }).notNull(),
+    amount: integer("amount").notNull(),
+    aiModel: aiModelEnum("ai_model").notNull(),
     userId: integer("user_id")
       .notNull()
       .references(() => users.id, {
