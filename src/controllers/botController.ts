a4c26654ef8a67fa7dba53be7a4d65790ed39a51
nkrivous <kolsln@list.ru>
2024-05-30 22:05:27 +1000

    Capture exceptions to Sentry

--- src/controllers/botController.ts
+++ src/controllers/botController.ts
@@ -6,6 +6,7 @@ import { bot } from "../bot/bot";
 import { repository, setup } from "src/setup";
 import { openai } from "src/bot/aiClients/openai";
 import { AI_MODEL } from "db/repository/aiModels";
+import * as Sentry from "@sentry/bun";
 
 export const BOT_CONTROLLER_PREFIX = "/bot";
 export const enum BOT_CONTROLLER_ROUTE {
@@ -33,14 +34,14 @@ export const botController = new Elysia({ prefix: BOT_CONTROLLER_PREFIX })
       console.log("Webhook", webhookUrl);
       return await bot.api.setWebhook(webhookUrl);
     } catch (e) {
-      console.error(e);
+      Sentry.captureException(e);
     }
   })
   .get(BOT_CONTROLLER_ROUTE.setCommands, ({ service }) => {
     try {
       service.setCommands();
     } catch (e) {
-      console.error(e);
+      Sentry.captureException(e);
     }
   })
   .post(BOT_CONTROLLER_ROUTE.update, async ({ body }) => {
@@ -48,7 +49,7 @@ export const botController = new Elysia({ prefix: BOT_CONTROLLER_PREFIX })
     try {
       await bot.handleUpdate(update);
     } catch (e) {
-      console.error(e);
+      Sentry.captureException(e);
     }
 
     return new Response("ok", { status: 200 });
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 import { Elysia } from "elysia";
 import { testController } from "./controllers/testController";
 import { botController } from "./controllers/botController";
+import * as Sentry from "@sentry/bun";
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  tracesSampleRate: 1.0,
+});
 
 export const app = new Elysia()
+  .onError(({ error }) => {
+    Sentry.captureException(error);
+    return new Response(error.toString());
+  })
   .use(botController)
   .use(testController)
   .get("/", () => "Test")
